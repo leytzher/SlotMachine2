e25578ebf28494c8baa50bc64b5aa33218f22240
leytzher <leytzher@gmail.com>
2015-01-20 20:33:24 +0800

    First Commit

--- SlotMachine2/ViewController.swift
+++ SlotMachine2/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // define containers as variables of UIView type.
+    var firstContainer: UIView!
+    var secondContainer : UIView!
+    var thirdContainer : UIView!
+    var fourthContainer : UIView!
+    
+    
+    //Global constants
+    let kMarginForView:CGFloat = 10.0  //CGFloat makes either a double or a float
+    let kSixth: CGFloat = 1.0/6.0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
